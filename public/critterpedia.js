c34bef7d25f4009972fdd9e3ed0bf13a165e1452
M.R0 <mzer0@MR0s-Air.home>
2022-04-17 15:26:37 -0500

    init commit

---
+++ public/critterpedia.js
@@ -0,0 +1,44 @@
+
+
+const clickCritter = (critter) =>{
+    let critterContent = document.getElementById('critr-content')
+    critterContent.classList.add('hide')
+    critterContent.classList.remove('sticky')
+    console.log('you clicked me')
+    axios.get(`/${critter}`).then((res) =>{
+        console.log('got critter')
+        console.log(res)
+        let dumpCritters = document.getElementById('dump-critters')
+        // dumpCritters.children.clear()
+        while (dumpCritters.firstChild){
+            dumpCritters.removeChild(dumpCritters.firstChild)
+        }
+        for(let i = 0; i < res.data.length; i++){
+            let critter = document.createElement('img')
+            critter.className = 'critter-item'
+            critter.src = res.data[i].image_url
+            critter.addEventListener('click', e =>{
+                let renderedImage = document.getElementById('rendered-image')
+                renderedImage.src = res.data[i].render_url
+                let availability = document.getElementById('availability')
+                let location = document.getElementById('location')
+                let price = document.getElementById('price')
+                availability.innerText = res.data[i].north.months 
+                location.innerText = res.data[i].location
+                price.innerText = res.data[i].sell_nook
+                dumpCritters.classList.add('hide')
+                let critterDetailContainer = document.getElementById('critter-detail-container')
+                critterDetailContainer.classList.remove('hide')
+            })
+            dumpCritters.appendChild(critter)
+        }
+    })
+}
+
+const clickBack = () =>{
+    let critterDetailContainer = document.getElementById('critter-detail-container')
+    critterDetailContainer.classList.add('hide')
+    let dumpCritters = document.getElementById('dump-critters')
+    dumpCritters.classList.remove('hide')
+}
+
---
+++ server.js
@@ -0,0 +1,44 @@
+const express =require('express')
+const app = express()
+const path = require('path')
+const {acnhApi} = require('./acnh-api')
+
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/public/index.html'))
+})
+app.get('/bugs',(req,res) => {
+    console.log('gettingbugs')
+    acnhApi.getBugs().then(response => {
+        console.log('got bugs')
+        console.log(response.data)
+        res.send(response.data)
+    }) 
+   
+    //  res.send(acnhApi.getBugs())
+})
+app.get('/fish',(req,res)=>{
+    console.log('gone fishing')
+    acnhApi.getFish().then(response =>{
+        console.log('got feesh')
+        console.log(response.data)
+        res.send(response.data)
+    })
+})
+
+app.get('/sea',(req,res)=>{
+    console.log('diving for sea critters')
+    acnhApi.getSea().then(response =>{
+        console.log(response.data)
+        res.send(response.data)
+    })
+})
+
+
+const port = process.env.PORT || 5050;
+
+app.listen(port,()=>{
+    console.log(`the server is LIIIIIIIVE on ${port}`)
+})
